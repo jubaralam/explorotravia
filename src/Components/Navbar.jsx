900b5ab570b330b3a05caacf843b47417fc84328
Zubair Alam <zubairalam@Zubairs-MacBook-Air.local>
2024-12-23 11:53:26 +0530

    project has initialized

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-evenly bg-slate-500 w-full">
+        <div className="logo">logo</div>
+        <div>
+          <ul className="flex p-2 m-2">
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+        </div>
+        <div>profile</div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
